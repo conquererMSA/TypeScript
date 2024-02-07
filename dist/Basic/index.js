ee04009f53882776629fd7651da123a5c9ad9fc5
asus test <msa-99@outlook.com>
2024-02-07 20:18:15 +0600

    initial setup

---
+++ dist/Basic/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const person = "MSA2";
+console.log(person);
---
+++ src/Basic/index.ts
@@ -0,0 +1,4 @@
+const person: string = "MSA2";
+console.log(person);
+const jailer: string = "movie";
+console.log(jailer);
